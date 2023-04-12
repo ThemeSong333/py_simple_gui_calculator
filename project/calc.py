648604d917a04de3e7674c0090564052564a7953
ThemeSong333 <themesong3alex@gmail.com>
2023-04-12 19:03:38 +0200

    added icon and added style changes

--- project/calc.py
+++ project/calc.py
@@ -1,22 +1,32 @@
 import PySimpleGUI as sg
-result_panel = sg.InputText(size=(20,1), key='input')
-button_0 = sg.Button("0")
-button_1 = sg.Button("1")
-button_2 = sg.Button("2")
-button_3 = sg.Button("3")
-button_4 = sg.Button("4")
-button_5 = sg.Button("5")
-button_6 = sg.Button("6")
-button_7 = sg.Button("7")
-button_8 = sg.Button("8")
-button_9 = sg.Button("9")
-button_equals = sg.Button("=")
-button_plus = sg.Button("+")
-button_divide = sg.Button("/")
-button_multiply = sg.Button("*")
-button_minus = sg.Button("-")
-button_dot = sg.Button(".")
-button_clear = sg.Button("C")
+
+# colors, fonts, sizes
+button_size = (4,2)
+pop_up_size = (40, 40)
+digits_color="blue"
+operator_color="green"
+clear_button_color="red"
+global_font = ("Consolas", 14)
+
+# elements of calculator
+result_panel = sg.InputText(size=(20,1), key='input', disabled=True)
+button_0 = sg.Button("0", size=button_size, button_color=digits_color)
+button_1 = sg.Button("1", size=button_size, button_color=digits_color)
+button_2 = sg.Button("2", size=button_size, button_color=digits_color)
+button_3 = sg.Button("3", size=button_size, button_color=digits_color)
+button_4 = sg.Button("4", size=button_size, button_color=digits_color)
+button_5 = sg.Button("5", size=button_size, button_color=digits_color)
+button_6 = sg.Button("6", size=button_size, button_color=digits_color)
+button_7 = sg.Button("7", size=button_size, button_color=digits_color)
+button_8 = sg.Button("8", size=button_size, button_color=digits_color)
+button_9 = sg.Button("9", size=button_size, button_color=digits_color)
+button_equals = sg.Button("=", size=button_size, button_color=operator_color)
+button_plus = sg.Button("+", size=button_size, button_color=operator_color)
+button_divide = sg.Button("/", size=button_size, button_color=operator_color)
+button_multiply = sg.Button("*", size=button_size, button_color=operator_color)
+button_minus = sg.Button("-", size=button_size, button_color=operator_color)
+button_dot = sg.Button(".", size=button_size, button_color=operator_color)
+button_clear = sg.Button("C", size=button_size, pad=(20), button_color=clear_button_color)
 
 current_value = ""
 calculation = ["", "", ""]
@@ -31,7 +41,7 @@ layout = [
 
 
 
-window = sg.Window('Calculator', layout)
+window = sg.Window('Calculator', layout, font=global_font, background_color="gray", icon='E:\code\Python\personal_projects\py_simple_gui_calculator\project\imgs\gears.ico')
 
 
 while True:
@@ -72,8 +82,11 @@ while True:
             current_value = str(eval(c))    
             calculation = [current_value, "", ""]
     except ZeroDivisionError:
-        sg.popup("You can not divide by zero", title="Error")
-        
+        sg.popup("You can not divide by zero", title="Error", font=global_font)
+    except SyntaxError:
+        sg.popup("You made an error, reseting calculator", title="Error", font=global_font)
+        current_value = ""
+        calculation = ["", "", ""]
         
     # update display
     window["input"].update(current_value)
